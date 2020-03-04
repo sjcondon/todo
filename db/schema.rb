fe0dde4153c4f3ff5e8f888438c9e8bb35e84e82
Sarah Condon <s.j.condon01@gmail.com>
2020-03-04 16:03:30 -0700

    working on views for todo_list

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_04_224009) do
+ActiveRecord::Schema.define(version: 2020_03_04_224432) do
+
+  create_table "todo_items", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "todo_lists", force: :cascade do |t|
     t.string "title"
