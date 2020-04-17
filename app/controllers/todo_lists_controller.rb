134e9a83b08a924477443ffc80d591d479c238f5
Sarah Condon <s.j.condon01@gmail.com>
2020-04-17 11:22:27 -0600

    fix scope method

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,5 +1,5 @@
 class TodoListsController < ApplicationController
-  # before_action :set_todo_list, only: [:show, :edit, :update, :destroy]
+  before_action :set_todo_list, only: [:show, :edit, :update, :destroy]
   
   def index
     @todo_lists = TodoList.all
