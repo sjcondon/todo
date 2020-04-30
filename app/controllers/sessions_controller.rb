afaba82510a97eab05b3fd99c30370fb75a8913d
Sarah Condon <s.j.condon01@gmail.com>
2020-04-30 10:35:25 -0600

    omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,9 @@ class SessionsController < ApplicationController
     end
 
     def create
+        @user = User.find_or_create_from_auth_hash(auth_hash)
+        self.current_user = @user
+        redirect_to '/'
         @user = User.find_by(:email => params[:email]) #find user by email typed in
         if @user
             #logging in a user
@@ -13,11 +16,18 @@ class SessionsController < ApplicationController
             # redirect_to "/login", :notice => "Cannot find that email"
             render :new
         end
+        
     end
 
     def destroy
         reset_session
         redirect_to "/"
+    end
+
+    protected
+
+    def auth_hash
+        request.env['omniauth.auth']
     end
 end
 
