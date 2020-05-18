16fbdf68966916c88b86ec42c2d6732cb986c525
Sarah Condon <s.j.condon01@gmail.com>
2020-05-18 09:48:39 -0600

    adding github to login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,24 +8,25 @@ class SessionsController < ApplicationController
         if auth_hash = request.env["omniauth.auth"]
             @user = User.find_or_create_by_omniauth(auth_hash)
             session[:current_user_id] = @user.id
-            
+
             redirect_to "/"
            
         elsif @user = User.find_by(:email => params[:email])
             #normal login with un and pw
              #find user by email typed in
         
-    
             if @user && @user.authenticate(params[:password])
                     session[:current_user_id] = @user.id
                     #logging in a user
-                    login(@user)
+                    #login(@user)
+            
                     redirect_to "/", :notice => "Log in Successful"
             else
                 # redirect_to "/login", :notice => "Cannot find that email"
                     render :new
             end      
-        else
+        else 
+            @error = "Couldn't find user, please try again"
             # redirect_to "/login", :notice => "Cannot find that email"
                 render :new
         end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
     validates :email, :uniqueness => true 
 
     def self.find_or_create_by_omniauth(auth_hash)
-        self.where(:email => auth_hash["info"]["email"]).first_or_create do |user|
+        self.where(:email => auth_hash["info"]["nickname"]).first_or_create do |user|
             user.password = SecureRandom.hex
 
         end
