43e82ba48a5d4939cf140706193dff1b45523974
Sarah Condon <s.j.condon01@gmail.com>
2020-06-06 23:32:19 -0600

    add comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :todo_lists
   resources :todo_lists do
-    resources :todo_items do
+    resources :todo_items do #loop through and will give the correct item (nested route)
     	member do
     		get :complete
       end
